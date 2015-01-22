8758c09752d8ebe062d25feb75cd46f960e37a14
Eirij Qureshi <eirij.qureshi@gmail.com>
2015-01-22 17:05:36 +0500

    non-blocking design using event handlers done

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,30 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
     console.log("Handle 'start'.");
+
+    var content = "empty";
+
+/*exec("find /",
+{ timeout: 10000, maxBuffer: 20000*1024 },
+function (error, stdout, stderr) {
+response.writeHead(200, {"Content-Type": "text/plain"});
+response.write(stdout);
+response.end();
+});*/
+
+    exec("ls -lah", function (error, stdout, stderr) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
+    });
 }
 
-function upload() {
+function upload(response) {
     console.log("Handle 'upload'.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end(); 
 }
 
 exports.start = start;
